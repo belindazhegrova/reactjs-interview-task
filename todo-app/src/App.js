d7dd428cc8ecd58b1f98d181164ad34bf3622ff8
belinda zhegrova <belindazhegrova1@gmail.com>
2024-11-09 23:41:23 +0100

    changes

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Row } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col span={12}>container 1</Col>
+      <Col span={12}>container 2</Col>
+    </Row>
   );
 }
 
